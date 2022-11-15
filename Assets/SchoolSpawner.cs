b2a63c3dd168ac7805ccede0a2f9cd3f4d266006
Ben Snow <56250881+BenSnow6@users.noreply.github.com>
2022-11-15 12:33:26 +0000

    Spawns schools in flyover

--- Assets/SchoolSpawner.cs
+++ Assets/SchoolSpawner.cs
@@ -7,12 +7,13 @@ public class SchoolSpawner : MonoBehaviour
     [SerializeField] GameObject schoolMarker;
     private Vector3 offset; // offset position of the tip of the marker prefab
     public TextAsset SchoolNameData; // csv file with location data
+    public Renderer map; // map renderer
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log($"MAPSIZE {map.bounds.size}");
         showSchools();
-        Debug.Log("Schools spawned");
         Instantiate(schoolMarker, offset+new Vector3(0,0,0), Quaternion.identity);
     }
 
@@ -21,12 +22,12 @@ public class SchoolSpawner : MonoBehaviour
     {
         
     }
-    void spawnSchool(Vector3 position, string schoolName)
+    void spawnSchool(Vector3 position, Vector3 offset, string schoolName)
     {
         GameObject schoolObject = Instantiate(schoolMarker, offset+position, Quaternion.identity) as GameObject;  // instatiate the object
         schoolObject.transform.localScale = new Vector3(300f, 300f, 300f); // set size of the prefabs
         // set rotation of the prefabs
-        schoolObject.transform.Rotate(90, 0, 0);
+        schoolObject.transform.Rotate(270, 0, 0);
         schoolObject.tag = "School"; // set tag for the object
         // Set child text object to school name
         schoolObject.transform.GetChild(0).GetComponent<TextMesh>().text = schoolName;
@@ -38,30 +39,28 @@ public class SchoolSpawner : MonoBehaviour
 
     void showSchools()
     {
-        float tipPosition_y = 0.256f-(256-222)/256; // 256-222 = 34 pixels from the bottom of the image
-        Vector3 offset = new Vector3(0, tipPosition_y, 2.73958f); // location offset of the marker's tip
+        float tipPosition_y = 2.5f;
+        Vector3 offset = new Vector3(0, tipPosition_y, 0); // location offset of the marker's tip
 
 
         string[] data = SchoolNameData.text.Split(new string[] {",", "\n"}, System.StringSplitOptions.None); //read the csv file
         for (int i = 0; i < data.Length; i++)
         {
-            float x_f = -1122.3f;
+            float x_f = 0;
             float x_g = 2224.6f;
             float x_d = -1.6f;
             float x_e = -1.28f;
             float y_f = 0f;
-            float y_g = 2*873.35f;
+            float y_g = map.bounds.size.z;
             float y_d = 50.863096f;
             float y_e = 51.02f;
-
-            Debug.Log(data[3*i]);
 
             // Scale the data to the grid bounds
             float x_pos = (x_f-x_g)/(x_d-x_e)*(float.Parse(data[3*i+1])-x_d)+x_f;
 
             float y_pos = (y_f-y_g)/(y_d-y_e)*(float.Parse(data[3*i+2])-y_d)+y_f;
 
-            spawnSchool(new Vector3(x_pos, 0, y_pos), data[i*3]); // instantiate prefabs at each of the locations
+            spawnSchool(new Vector3(x_pos, 0, y_pos), offset, data[i*3]); // instantiate prefabs at each of the locations
         }
         /*
         read in csv
